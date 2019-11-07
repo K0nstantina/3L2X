29eb386296b0a3ba7a2872f66f62f0492f4c5612
Thodoris Zervakis <th.zervakis@outlook.com>
2019-11-07 15:09:49 +0200

    gia kosta
    checkedListBox

---
+++ Kostas/testingMainDB/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace testingMainDB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainPanel = new System.Windows.Forms.Panel();
+            this.selectedIndexesLabel = new System.Windows.Forms.Label();
+            this.mainPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.Controls.Add(this.selectedIndexesLabel);
+            this.mainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainPanel.Location = new System.Drawing.Point(0, 0);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(652, 450);
+            this.mainPanel.TabIndex = 0;
+            // 
+            // selectedIndexesLabel
+            // 
+            this.selectedIndexesLabel.Location = new System.Drawing.Point(272, 0);
+            this.selectedIndexesLabel.Name = "selectedIndexesLabel";
+            this.selectedIndexesLabel.Size = new System.Drawing.Size(380, 450);
+            this.selectedIndexesLabel.TabIndex = 3;
+            this.selectedIndexesLabel.Text = "selected: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(652, 450);
+            this.Controls.Add(this.mainPanel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.mainPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel mainPanel;
+        private System.Windows.Forms.Label selectedIndexesLabel;
+    }
+}
+
---
+++ Kostas/testingMainDB/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace testingMainDB
+{
+    public partial class Form1 : Form
+    {
+        // Connection String and Query
+        string connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\\Users\\TEO\\Downloads\\AggeliesDatabase.accdb";
+        string selectQuery = "SELECT * FROM Extras";
+        OleDbCommand command;
+        // Create the CheckedListBox 
+        CheckedListBox extrasCheckedListBox = new CheckedListBox();
+        public Form1()
+        {
+            // Set checkOnClick and adding event listener
+            extrasCheckedListBox.CheckOnClick = true;
+            extrasCheckedListBox.SelectedIndexChanged += new System.EventHandler(extrasCheckedListBox_SelectedIndexChanged);
+            // Form init
+            InitializeComponent();
+            // Dock and Add to mainPanel
+            extrasCheckedListBox.Dock = DockStyle.Left;
+            mainPanel.Controls.Add(extrasCheckedListBox);
+            // Start the connection
+            using (OleDbConnection connection = new OleDbConnection(connectionString))
+            {
+                command = new OleDbCommand(selectQuery, connection);
+                connection.Open();
+                OleDbDataReader reader = command.ExecuteReader();
+                // For each entry add to list
+                while (reader.Read())
+                {
+                    //populate the CheckedListBox
+                    extrasCheckedListBox.Items.Add(reader["exTitle"].ToString());
+                }
+                // Close reading.
+                reader.Close();
+                connection.Close();
+            }
+        }
+        private void extrasCheckedListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Get selected value
+            string str = extrasCheckedListBox.SelectedItem.ToString();
+            // If checked add to selectedIndexesLabel
+            if (extrasCheckedListBox.GetItemChecked(extrasCheckedListBox.SelectedIndex))
+            {
+                selectedIndexesLabel.Text += " " + str;
+            }
+            else
+            {
+                selectedIndexesLabel.Text.Replace(str, " ");
+            }
+        }
+    }
+}
