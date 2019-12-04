7cb37c0c369942ed1eb5ef518cacb30dc1aa3d46
PetrellisKostas <48026178+PetrellisKostas@users.noreply.github.com>
2019-12-04 20:01:57 +0200

    removed userMenu in user application project

--- Aggelies3L2X/AdministratorPanel/AdminForm.cs
+++ Aggelies3L2X/AdministratorPanel/AdminForm.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,24 +15,6 @@ namespace AdministratorPanel
         public AdminForm()
         {
             InitializeComponent();
-        }
-
-        
-
-        private void Button1_Click(object sender, EventArgs e)
-        {
-            ChangeLang("el");
-        }
-
-        private void ChangeLang(string lang)
-        {
-            CultureInfo ci = new CultureInfo(lang);
-
-            System.Resources.ResourceManager rm = new System.Resources.ResourceManager("AdministratorPanel.MyResources.Labels", typeof(AdminForm).Assembly);
-            label1.Text = rm.GetString("Authentication", ci);
-            button1.Text = rm.GetString("Button", ci);
-           
-            this.Text = rm.GetString("Authentication", ci);
         }
     }
 }
